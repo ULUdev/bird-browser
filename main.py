0b6363e2456f9d52307a329927eb8e7052c4d97a
moritz <moritz@sokoll.com>
2020-10-03 14:09:58 +0200

    "added readme"

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+import sys
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("browser")
+        self.browser = QWebView()
+        self.browser.setUrl(QUrl("https://moritz.sokoll.com"))
+        self.setCentralWidget(self.browser)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
