b4e083996fb5bf5f4041e8cc28aa95678b999473
moritz <moritz@sokoll.com>
2020-10-24 21:35:29 +0200

    test

--- bird-browser/birdlib/extension.py
+++ bird-browser/birdlib/extension.py
@@ -3,6 +3,7 @@ class Extension:
     def __init__(self, name:str, version:str, browser=None):
         self._name = name
         self._version = version
+        self.browser = browser
 
     def __repr__(self):
         return f"Extension {self._name} version {self._version}"
